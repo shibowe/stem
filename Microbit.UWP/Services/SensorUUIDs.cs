e3de5e682163e1ce348e807f0ac5b6960377ba6f
WinEcos <dev@winecos.com>
2017-08-03 12:46:16 +0800

    add foreground color of tblresult

---
+++ Microbit.UWP/Services/SensorUUIDs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microbit.UWP.Services
+{
+    public static class SensorUUIDs
+    {
+        public static String UARTSERVICE_SERVICE_UUID = "6E400001B5A3F393E0A9E50E24DCCA9E";
+        public static String UART_RX_CHARACTERISTIC_UUID = "6E400002B5A3F393E0A9E50E24DCCA9E";
+        public static String UART_TX_CHARACTERISTIC_UUID = "6E400003B5A3F393E0A9E50E24DCCA9E";
+
+        public const string UUID_ACC_SERV = "02366e80-cf3a-11e1-9ab4-0002a5d5c51b";
+        public const string UUID_ACC_DATA = "340a1b80-cf4b-11e1-ac36-0002a5d5c51b";
+        public const string UUID_ENV_SERV = "42821a40-e477-11e2-82d0-0002a5d5c51b";
+        public const string UUID_ENV_TEMP = "a32e5520-e477-11e2-a9e3-0002a5d5c51b";
+        public const string UUID_ENV_PRES = "cd20c480-e48b-11e2-840b-0002a5d5c51b";
+    }
+}
---
+++ Microbit.UWP/Views/DeviceSensorPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using Microbit.UWP.ViewModels;
+using Windows.UI.Core;
+using Windows.Devices.Enumeration;
+using Microbit.UWP.Services;
+
+namespace Microbit.UWP.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class DeviceSensorPage : Page
+    {
+        SensorsViewModel viewModel;
+        public DeviceSensorPage()
+        {
+            this.InitializeComponent();
+        }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
+            SystemNavigationManager.GetForCurrentView().BackRequested += DevicePage_BackRequested;
+
+            var device = (e.Parameter as DeviceInformationDisplay).DeviceInformation;
+
+            viewModel = new SensorsViewModel(device);
+            viewModel.StartReceivingData();
+
+
+            this.DataContext = viewModel;
+            base.OnNavigatedTo(e);
+        }
+        private void DevicePage_BackRequested(object sender, BackRequestedEventArgs e)
+        {
+            if (this.Frame.CanGoBack)
+            {
+                this.Frame.GoBack();
+            }
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            SystemNavigationManager.GetForCurrentView().BackRequested -= DevicePage_BackRequested;
+            viewModel.Dispose();
+            base.OnNavigatedFrom(e);
+        }
+    }
+}
